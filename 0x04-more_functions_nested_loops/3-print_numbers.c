95743356a84184c3e6fe6367483eee79a8e7a469
wizkiye <wizkiye@gmail.com>
2022-03-18 00:22:23 +0300

    more functions and loops in c (7)

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * print_numbers - Entry Point
- * Return:numbers
+ * print_numbers - print numbers
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
-	int n = 0;
+    int i;
 
-	while (n <= 9)
-	{
-		_putchar(n + 48);
-		n++;
-	}
-	_putchar('\n');
+    i = 0;
+    while (i < 10)
+    {
+    _putchar (i + '0');
+    i++;
+    }
+
+    _putchar ('\n');
 }
