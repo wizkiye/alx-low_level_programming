72354a5af06a12171567dda7a6a89e4a2c53ccd4
wizkiye <wizkiye@gmail.com>
2022-03-18 00:00:23 +0300

    more functions and loops in c (2)

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,11 @@
 
 /**
  * mul - multiplies two integers
+ *
  * @a: int to be multiplied to b
+ *
  * @b: int to be multiplied to a
+ *
  * Return: the result of the operation
  */
 int mul(int a, int b)
