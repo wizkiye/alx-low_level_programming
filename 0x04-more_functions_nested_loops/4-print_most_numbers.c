b81adb809fa83cd7d15667c4c8270e2947d2f221
wizkiye <wizkiye@gmail.com>
2022-03-18 00:32:48 +0300

    more functions and loops in c (8)

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * print_most_numbers - prints the numbers, from 0 to 9,
- * except 2 and 4, followed by a new line
+ * print_most_numbers - Entry Point
+ * Return:numbers
  */
 void print_most_numbers(void)
 {
-	int i;
+	int n = 0;
 
-	for (i = 0; i < 10; i++)
+	while (n <= 9)
 	{
-		if (i != 2 && i != 4)
+		if (n != 2 && n != 4)
 		{
-			_putchar(i + '0');
+			_putchar('0' + n);
 		}
+		n++;
 	}
-
 	_putchar('\n');
 }
