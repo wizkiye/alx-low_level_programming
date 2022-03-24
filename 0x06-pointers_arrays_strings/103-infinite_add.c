bc9951c90772883361f3f48714147cf61872e85a
wizkiye <wizkiye@gmail.com>
2022-03-24 11:37:18 +0300

    fixed some mistake

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * infinite_add - adds arbitrarily long string of digits
+ * @n1: the first digit string
+ * @n2: the second digit string
+ * @r: the result buffer
+ * @size_r: the size of result buffer
+ *
+ * Return: char pointer to buffer
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
